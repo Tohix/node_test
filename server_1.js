9cbc53c5af892f944172702555fcb05fca5ac43d
Anton Yastrebov <tohix.anton@gmail.com>
2017-09-13 19:20:40 +0300

    new file server_1

---
+++ server_1.js
@@ -0,0 +1,37 @@
+/**
+ * Created by antonyastrebov on 9/13/17.
+ */
+var http = require('http');
+var url = require('url');
+
+// var server = new http.Server();
+//
+// server.listen(1337, '127.0.0.1');
+//
+// var counter = 0;
+// server.on('request', function(req, res) {
+//     res.end('Hello world! ' + ++counter);
+// })
+///-------------------
+
+
+var server = new http.Server(function(req, res) {
+
+    console.log(req.headers);
+
+
+    var urlParsed = url.parse(req.url, true);
+    console.log(urlParsed);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.setHeader('Cache-control', 'no-cache');
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
+});
+
+server.listen(1337, '127.0.0.1');
+
+console.log('server is running');
